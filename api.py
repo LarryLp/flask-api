80f6e074d7d773137d10287788f1bd3840caee0d
LarryLp <jiang-gq@msn.com>
2020-04-29 20:35:19 +0800

    Update api.py

--- api.py
+++ api.py
@@ -615,4 +615,4 @@ if __name__ == "__main__":
     scheduler = APScheduler()
     scheduler.init_app(app)
     scheduler.start()
-    app.run(host='localhost', debug=True)
+    app.run(host='0.0.0.0', debug=True)
